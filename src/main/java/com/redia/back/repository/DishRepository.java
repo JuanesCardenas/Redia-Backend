89ba792b20844ea9b175d38d8e9572d5995eb6fe
Cel18 <celeste.buitragor@uqvirtual.edu.co>
2026-04-17 21:29:10 -0500

    Detalle del plato

--- src/main/java/com/redia/back/repository/DishRepository.java
+++ src/main/java/com/redia/back/repository/DishRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface DishRepository extends JpaRepository<Dish, Long> {
+public interface DishRepository extends JpaRepository<Dish, String> {
 
     /** Obtener todos los platos disponibles del menú */
     List<Dish> findByAvailableTrue();
